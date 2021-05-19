62a60a3408ee89fd6b32ec58316e244d1544411b
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2021-05-19 19:29:37 -0400

    useless change

--- Assets/Scripts/Bowling/Kegel.cs
+++ Assets/Scripts/Bowling/Kegel.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Kegel : MonoBehaviour
 {
+    //private Vector3 startPosition;
+    //private Quaternion startRotation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //startPosition = this.transform.position;
+        //startRotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    // if fall down, add to score ?
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("ball"))
+        {
+            Debug.Log("hit: " + this.name);
+            //other.gameObject.SetActive(false);
+            //count++;
+            //SetCountText();
+
+            // Debug.Log(other.gameObject.name);
+            // Debug.Log(count);
+        }
+
     }
 }
