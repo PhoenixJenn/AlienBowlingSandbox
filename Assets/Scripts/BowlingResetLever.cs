3a3cb5e98a7ecbb5e2309a5688a40c852f14a45e
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2021-05-20 07:44:20 -0400

    Reset Bowling Trigger

---
+++ Assets/Scripts/BowlingResetLever.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingResetLever : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Hand"))
+        {
+            Debug.Log("Reset Game!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hand") {
+            Debug.Log("Reset Game 1!");
+        }
+    }
+}
